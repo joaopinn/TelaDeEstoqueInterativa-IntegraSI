082648e9ffab4f453a495d06ae4b111c2369e190
CarlosH-Santaigo <carloshsantiago360@gmail.com>
2026-08-18 15:06:11 -0300

    repositorio preparado para oficina

--- vite.config.ts
+++ vite.config.ts
@@ -33,4 +33,13 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 1000,
+      ignored: ['**/node_modules/**', '**/.git/**', '**/dist/**'],
+    },
+  },
 })
+
+
